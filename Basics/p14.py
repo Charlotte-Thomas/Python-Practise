b9b9c6d58046e7f786d43f116254c804527d3302
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-05-08 16:43:55 +0100

    loops

--- Basics/p14.py
+++ Basics/p14.py
@@ -6,7 +6,7 @@
 
 # ------- Inputs --------
 
-# p.14 - 011 enter a num > 100 and then a num < 10 & tell user how many times the smaller num goes into the larger
+# | p.14 - 011 |
 num1 = int(input('enter a number larger than 100: '))
 num2 = int(input('enter a number smaller than 10: '))
 division = num1 // num2
@@ -14,7 +14,7 @@ print(num2, ' goes into ', num1, '', division, ' times')
 
 # ------- If Statements --------
 
-# p.21 = 0.19 enter 1, 2 or 3. if 1 print "thank you", if 2 "well done" & if 3 "correct" - Else "error message"
+# | p.21 - 0.19 |
 num = int(input("Enter 1, 2 or 3: "))
 if num == 1:
     print("thank you")
@@ -28,7 +28,6 @@ else:
 #  ------- Strings --------
 
 # EXTRA :
-
 # to strip certain letter / spaces from  string:
 string = "hello world"
 print(string.strip("h"))
@@ -36,12 +35,9 @@ print(string.strip("h"))
 # only includes specified letter indexes in string (spaces counted as indexes):
 print(string[7:10])  # inlcudes 7, 8 & 9 only
 
-# p.28 - 026 change input into pig latin
+# | p.28 - 026 |
 string = input("enter a word: ").lower()
 if string.startswith("a" or "e" or "i" or "o" or "u"):
     print(string + "way")
 else:
     print(string[1:len(string)] + string[0] + "ay")
-
-
-
--- Basics/p31.py
+++ Basics/p31.py
@@ -1,16 +1,13 @@
 
-import math
+# import math
 
 #  ------- Maths --------
 
 # EXTRA:
-
 # to round to a certain no. of decimal places:
 print(round(12.3333, 2))  # rounds to 2 dps
 
-# p.32 - 034 print "1) Square 2) Triangle", let user choose num.
-# If 1: ask for one length of the square & display area, if 2: ask for base & height & display area else display error message.
-
+# | p.32 - 034 |
 print("1) Square \n2) Triangle")
 choice = int(input("choose option 1 or 2: "))
 if choice == 1:
@@ -22,3 +19,31 @@ elif choice == 2:
     print("area of triangle = ", (base * height) / 2)
 else:
     print("invalid choice")
+
+
+#  ------- For Loops --------
+
+# EXTRA:
+# first arg is start num, 2nd is range, 3rd is how much is added (or subtracted) to i every loop
+for i in range(1, 10, 2):
+    print(i)
+# you can also loop through strings
+
+# | p.37 - 044 |
+people = int(input("number of people invited: "))
+if people < 10:
+    for i in range(0, people):
+        name = input("name of person: ")
+        print(name, "has been invited: ")
+else:
+    print("too many people")
+
+
+#  ------- While Loops --------
+
+# EXTRA:
+# while loop where input decides when it ends
+carry_on = "yes"
+while carry_on == "yes":
+    print(":)")
+    carry_on = input("carry on? yes / no: ")
