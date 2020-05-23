da3a4ab355f7d5627ea87edc77f8a68168dabdf0
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-05-23 17:22:06 +0100

    params practise

---
+++ Tests/test_parametrize.py
@@ -0,0 +1,22 @@
+
+# pytest Tests/test_parametrize.py::test_add
+# test add
+
+import pytest
+
+
+# test add
+
+def add(x, y):
+    return x + y
+
+
+@pytest.mark.parametrize('x,y,expected', [
+    (3, 5, 8),
+    ('Hello', ' World', 'Hello World'),
+    (5.5, 6.5, 12)
+])
+def test_add(x, y, expected):
+    assert add(x, y) == expected
+
+
