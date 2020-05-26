e14543b19f21742bcae622527684c843dca22a5b
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-05-26 14:11:02 +0100

    temp file removed

--- Basics/C/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-    # new_array = [] # can't be numeric array because it won't accept rounded numbers
-    # for choice in choices:
-    #     divided = round(choice / user_num, 2)
-    #     print(divided)
-    #     new_array.append(divided)
-    # print(new_array)
-    # return new_array
