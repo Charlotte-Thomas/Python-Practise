8229dfeae62e1166b99384276c5bb523a067c69c
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-01 15:40:28 +0100

    books_by_year function

--- Basics/D/D2_p91.py
+++ Basics/D/D2_p91.py
@@ -18,7 +18,7 @@ def create_csv():
     years = ['1960', '1988', '1922', '1985', '1813']
 
     for i in range(0, 4):
-        file.write(books[i] + ', ' + authors[i] + ', ' + years[i] + '\n')
+        file.write(books[i] + ',' + authors[i] + ',' + years[i] + '\n')
     file.close()
 
 
@@ -29,7 +29,7 @@ def append_csv():
     book_name = str(input('Enter a book name: '))
     author = str(input('Enter author name: '))
     date = str(input('Enter year released: '))
-    file.write(book_name + ', ' + author + ', ' + date + '\n')
+    file.write(book_name + ',' + author + ',' + date + '\n')
     file.close()
 
     file = open('CSVs/Books.csv', 'r')
@@ -37,3 +37,16 @@ def append_csv():
     for row in reader:
         print(row)
     file.close()
+
+
+# | p.95 - 114 |
+
+def books_by_year():
+    start_year = int(input('enter a start year: '))
+    end_year = int(input('enter an end year: '))
+    file = open('CSVs/Books.csv', 'r')
+    reader = list(csv.reader(file))
+
+    for row in reader:
+        if start_year <= int(row[2]) <= end_year:
+            print(row)
