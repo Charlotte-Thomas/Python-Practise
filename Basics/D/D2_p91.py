b6abac37d75bd85f402c05ea60edda3a5e512b9a
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-01 17:56:54 +0100

    overwrite_csv function

--- Basics/D/D2_p91.py
+++ Basics/D/D2_p91.py
@@ -17,7 +17,7 @@ def create_csv():
     authors = ['Harper Lee', 'Stephen Hawking', 'F. Scott Fitzgerald', 'Oliver Sacks', 'Jane Austen']
     years = ['1960', '1988', '1922', '1985', '1813']
 
-    for i in range(0, 4):
+    for i in range(0, 5):
         file.write(books[i] + ',' + authors[i] + ',' + years[i] + '\n')
     file.close()
 
@@ -50,10 +50,36 @@ def books_by_year():
     for row in reader:
         if start_year <= int(row[2]) <= end_year:
             print(row)
+    file.close()
 
+
+# | p.95 - 115 |
 
 def display_data():
     file = open('CSVs/Books.csv', 'r')
     reader = list(csv.reader(file))
     for row in enumerate(reader):
         print(f'row {row[0]}: {row[1]}')
+    file.close()
+
+
+# | p.95 - 116 |
+
+def overwrite_csv():
+    file = open('CSVs/Books.csv', 'r')
+    reader = list(csv.reader(file))
+    copy = reader[:]
+    display_data()
+    delete = int(input('Enter the row number you want to delete: '))
+    edit = int(input('enter the row number you want to edit: '))
+    section = int(input('enter number of section you want to edit (0-2): '))
+    new_data = input('enter new data: ')
+    copy[edit][section] = new_data
+    copy.pop(delete)
+    file.close()
+
+    file = open('CSVs/Books.csv', 'w')
+    for row in copy:
+        file.write(','.join(row) + '\n')
+    file.close()
+    display_data()
