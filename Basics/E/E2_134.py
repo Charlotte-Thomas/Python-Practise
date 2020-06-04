a924752ec31a8b9c7569080655e2f068ded93a4e
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-04 20:15:46 +0100

    phone book functions

--- Basics/E/E2_134.py
+++ Basics/E/E2_134.py
@@ -8,7 +8,7 @@ import sqlite3
 # EXTRA:
 # data types: integer, real(float), text, blob(stored exactly as it was input)
 
-# | p.139 - 139 |
+# | p.140 - 139 |
 
 def create_phonebook():
     with sqlite3.connect('PhoneBook.db') as db:
@@ -30,3 +30,55 @@ def create_phonebook():
         db.commit()
 
     db.close()
+
+
+# | p.140 - 140 |
+
+def view_phonebook(cursor):
+    cursor.execute('SELECT * FROM  PhoneBook')
+    for x in cursor.fetchall():
+        print(x)
+
+def insert_phonebook(cursor, db):
+    newId = input('enter ID number: ')
+    first_name = input('enter first name: ')
+    surname = input('enter surname: ')
+    number = input('enter phone number: ')
+    cursor.execute('''INSERT INTO PhoneBook(id, first_name, surname, number)
+      VALUES(?,?,?,?)''', (newId, first_name, surname, number))
+    db.commit()
+
+def search(cursor):
+    surname = input('enter surname to search for: ')
+    cursor.execute('SELECT * FROM PhoneBook WHERE surname=?', [surname])
+    print(cursor.fetchall())
+
+def del_person(cursor, db):
+    person = input('enter the ID of person you want to delete: ')
+    cursor.execute(f'DELETE FROM PhoneBook WHERE id={person}')
+    db.commit()
+
+def phonebook_options():
+    with sqlite3.connect('PhoneBook.db') as db:
+        cursor = db.cursor()
+
+    print('''    1) View phone book
+    2) Add to phone book
+    3) Search for surname
+    4) Delete person from phone book
+    5) Quit ''')
+
+    choice = int(input('choose a number from the selection: '))
+    if choice == 1:
+        view_phonebook(cursor)
+    elif choice == 2:
+        insert_phonebook(cursor, db)
+    elif choice == 3:
+        search(cursor)
+    elif choice == 4:
+        del_person(cursor, db)
+    elif choice == 5:
+        return
+    else:
+        raise Exception('not a choice')
+    db.close()5
