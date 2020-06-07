81dfd7245efdff7a990ff710a77f4b174a9ed98a
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-07 15:54:06 +0100

    refactored code

--- ChunkyChallenges/Shift_Code.py
+++ ChunkyChallenges/Shift_Code.py
@@ -15,15 +15,24 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 skip = [' ', '\'', '.', ',', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 
-def convert_msg():
-    msg = input('enter message to be coded: ')
+def find_char(decode, char, num):
+    if not decode:
+        new_position = alphabet.index(char) + num
+        if new_position >= 26:
+            new_position -= 26
+    else:
+        new_position = alphabet.index(char) - num
+        if new_position <= -1:
+            new_position += 26
+    return new_position
+
+def convert_msg(decode):
+    msg = input('enter message to be coded / decoded: ')
     num = int(input('enter number (1 - 25) to shift message by: '))
     code = []
     for char in list(msg):
         if not skip.__contains__(char):
-            new_position = alphabet.index(char) + num
-            if new_position >= 26:
-                new_position -= 26
+            new_position = find_char(decode, char, num)
             code.append(alphabet[new_position])
         else:
             code.append(char)
@@ -34,7 +43,9 @@ def present_options():
     print(' 1) Make a code \n 2) Decode a message \n 3) Quit')
     choice = input('choose an option: ')
     if choice == '1':
-        convert_msg()
+        convert_msg(False)
+    if choice == '2':
+        convert_msg(True)
 
 
 present_options()
