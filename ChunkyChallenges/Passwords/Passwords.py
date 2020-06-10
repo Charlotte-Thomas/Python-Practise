cc0bd82a72b4c870fdf863f97f53b367888f3c7b
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-10 16:28:09 +0100

    Passwords started

---
+++ ChunkyChallenges/Passwords/Passwords.py
@@ -0,0 +1,30 @@
+
+import csv
+# | p.156 - 148 |
+
+# display options to user in while loop
+# function for each option and subfunctions to use across each (get data, password creation, find user, write data)
+
+
+def read_data():
+    file = open('ChunkyChallenges/Passwords/Passwords.csv', 'r')
+    reader = list(csv.reader(file))
+    for row in reader:
+        print(row)
+
+read_data()
+
+def create_user():
+    name = input('enter a user ID: ')
+
+
+def display_options():
+    # repeat = True
+    # while repeat:
+    print(' 1) Create a new User ID \n 2) Change a password \n 3) Display all User IDs \n 4) Quit')
+    choice = int(input('choose an option from above: '))
+    if choice == 1:
+        create_user()
+
+
+# display_options()
